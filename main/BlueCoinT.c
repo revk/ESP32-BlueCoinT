6b32903c0c8508d6bf0ae69e8b2b390105c59a76
RevK <github@k.gg>
2023-02-10 08:57:57 +0000

    Reporting now

--- main/BlueCoinT.c
+++ main/BlueCoinT.c
@@ -21,18 +21,6 @@ const char TAG[] = "BlueCoinT";
 
 #include <driver/gpio.h>
 
-#ifdef	CONFIG_LWIP_DHCP_DOES_ARP_CHECK
-#warning CONFIG_LWIP_DHCP_DOES_ARP_CHECK means DHCP is slow
-#endif
-#ifndef	CONFIG_LWIP_DHCP_RESTORE_LAST_IP
-#warning CONFIG_LWIP_DHCP_RESTORE_LAST_IP may improve speed
-#endif
-#ifndef	CONFIG_BOOTLOADER_SKIP_VALIDATE_IN_DEEP_SLEEP
-#warning CONFIG_BOOTLOADER_SKIP_VALIDATE_IN_DEEP_SLEEP may speed boot
-#endif
-#if	CONFIG_BOOTLOADER_LOG_LEVEL > 0
-#warning CONFIG_BOOTLOADER_LOG_LEVEL recommended to be no output
-#endif
 #ifndef	CONFIG_SOC_BLE_SUPPORTED
 #error	You need CONFIG_SOC_BLE_SUPPORTED
 #endif
@@ -46,6 +34,8 @@ httpd_handle_t webserver = NULL;
 
 #define	settings		\
 	u32(missingtime,30)	\
+	u32(reporting,60)	\
+	u8(temprise,50)		\
 
 #define u32(n,d)        uint32_t n;
 #define s8(n,d) int8_t n;
@@ -60,7 +50,6 @@ settings
 #undef u8
 #undef b
 #undef s
-
 const char *app_callback(int client, const char *prefix, const char *target, const char *suffix, jo_t j)
 {
    char value[1000];
@@ -74,16 +63,20 @@ const char *app_callback(int client, const char *prefix, const char *target, con
       if (len > sizeof(value))
          return "Too long";
    }
+   // TODO pick up other reports to know if we are closer
+
    if (client || !prefix || target || strcmp(prefix, prefixcommand) || !suffix)
       return NULL;              //Not for us or not a command from main MQTT
    if (!strcmp(suffix, "connect"))
-      lwmqtt_subscribe(revk_mqtt(0), "BlueCoinT/#");
+   {
+      lwmqtt_subscribe(revk_mqtt(0), "info/BlueCoinT/#");
+   }
    if (!strcmp(suffix, "shutdown"))
       httpd_stop(webserver);
    if (!strcmp(suffix, "upgrade"))
    {
       esp_bt_controller_disable();      // Kill bluetooth during download - TODO may be better done in RevK library, with a watchdog change
-      esp_wifi_set_ps(WIFI_PS_NONE);    // Fulk wifi
+      esp_wifi_set_ps(WIFI_PS_NONE);    // Full wifi
       revk_restart("Download started", 10);     // Restart if download does not happen properly
    }
    return NULL;
@@ -95,18 +88,15 @@ typedef struct device_s device_t;
 struct device_s {
    device_t *next;              // Linked list
    ble_addr_t addr;             // Address (includes type)
+   uint8_t namelen;             // Device name length
+   char name[32];               // Device name (null terminated)
    int8_t rssi;                 // RSSI
-   uint8_t len;                 // data len
-   uint8_t data[31];            // data (adv)
    uint32_t last;               // uptime of last seen
-   int16_t temp;		// Temp
-   uint8_t new:1;               // Should try a connection
-   uint8_t connected:1;         // We have seen a connection
-   uint8_t missing:1;           // Missing
-   uint8_t found:1;             // Found
-   uint8_t apple:1;             // Found apple
-   uint8_t nearby:1;            // Found apple nearby adv
-   // TODO what BlueCoinT
+   uint32_t lastreport;         // uptime of last reported
+   int16_t temp;                // Temp
+   int16_t tempreport;          // Temp last reported
+   uint8_t found:1;
+   uint8_t missing:1;
 };
 device_t *device = NULL;
 
@@ -117,14 +107,13 @@ static const char *ble_addr_format(ble_addr_t * a)
 {
    static char buf[30];
    snprintf(buf, sizeof(buf), "%02X:%02X:%02X:%02X:%02X:%02X", a->val[5], a->val[4], a->val[3], a->val[2], a->val[1], a->val[0]);
-   if (a->type == BLE_ADDR_PUBLIC)
-      strcat(buf, "(pub)");
-   else if (a->type == BLE_ADDR_RANDOM)
+   if (a->type == BLE_ADDR_RANDOM)
       strcat(buf, "(rand)");
    else if (a->type == BLE_ADDR_PUBLIC_ID)
       strcat(buf, "(pubid)");
    else if (a->type == BLE_ADDR_RANDOM_ID)
       strcat(buf, "(randid)");
+   //else if (a->type == BLE_ADDR_PUBLIC) strcat(buf, "(pub)");
    return buf;
 }
 
@@ -139,7 +128,6 @@ device_t *find_device(ble_addr_t * a)
       d = malloc(sizeof(*d));
       memset(d, 0, sizeof(*d));
       d->addr = *a;
-      d->new = 1;
       d->next = device;
       device = d;
       d->found = 1;
@@ -162,60 +150,36 @@ static int ble_gap_event(struct ble_gap_event *event, void *arg)
          const uint8_t *p = event->disc.data,
              *e = p + event->disc.length_data;
          if (e > p + 31)
-            break;
-         device_t *d = find_device(&event->disc.addr);
-         d->rssi = event->disc.rssi;
-         if (d->len == event->disc.length_data && !memcmp(d->data, event->disc.data, d->len))
-            break;
-         d->len = event->disc.length_data;
-         if (d->len)
-            memcpy(d->data, event->disc.data, d->len);
-         char msg[200],         // Message size very limited so always OK
-         *o = msg;
+            break;              // Silly
+         // Check if a temp device
+         const uint8_t *name = NULL;
+         const uint8_t *temp = NULL;
+         //const uint8_t *bat = NULL;
          while (p < e)
-	 {
+         {
             const uint8_t *n = p + *p + 1;
             if (n > e)
                break;
-            const uint8_t *P = p + 1;
-	    o+=sprintf(o,"%02X:",*P++);
-	    if(p[1]==8||p[1]==9)
-	    { // Name short or full
-	       o+=sprintf(o,"\"");
-	       while(P<n)o+=sprintf(o,"%c",*P++);
-	       o+=sprintf(o,"\"");
-	    }else if(p[1]==0x16)
-	    { // 16 bit UUID and value
-	       if(P[0]==0x6E&&P[1]==0x2A&&n==P+4)
-	       { // Temp
-		  int16_t v=((P[3]<<8)|P[2]);
-		  d->temp=v;
-		  if(v<0)
-		  {
-	             o+=sprintf(o,"-");
-	             v=-v;
-		  }
-		  o+=sprintf(o,"%d.%02d℃",v/100,v%100);
-	       }else
-	       {
-                  o+=sprintf(o,"%02X%02X=",P[0],P[1]);
-	          P+=2;
-	          while(P<n)
-	             o+=sprintf(o,"%02X",*P++);
-	       }
-	    }else
-	    { // Other data just hex dumped
-	       while(P<n)o+=sprintf(o,"%02X",*P++);
-	    }
-	    o+=sprintf(o," ");
-	    p=n;
-	 }
-         if (o == msg)
-            break;
-         if (o[-1] == ' ')
-            o--;
-         *o++ = 0;
-         ESP_LOGI(TAG, "Disc event %d, rssi %d, %s %s", event->disc.event_type, event->disc.rssi, ble_addr_format(&d->addr), msg);
+            if (p[0] > 1 && (p[1] == 8 || p[1] == 9))
+               name = p;
+            else if (*p == 5 && p[1] == 0x16 && p[2] == 0x6E && p[3] == 0x2A)
+               temp = p;
+            // TODO bat
+            p = n;
+         }
+         if (!temp || !name)
+            break;              // Not temp device
+         device_t *d = find_device(&event->disc.addr);
+         if (d->namelen != *name - 1 || memcmp(d->name, name + 2, d->namelen))
+         {
+            memcpy(d->name, name + 2, d->namelen = *name - 1);
+            d->name[d->namelen] = 0;
+         }
+         if (temp)
+            d->temp = ((temp[5] << 8) | temp[4]);
+         // TODO bat
+         d->rssi = event->disc.rssi;
+         ESP_LOGI(TAG, "Temp %s %d", d->name, d->temp);
          break;
       }
 
@@ -282,6 +246,8 @@ void app_main()
 #undef s
        revk_start();
 
+   revk_wait_mqtt(60);
+
    REVK_ERR_CHECK(esp_wifi_set_ps(WIFI_PS_MIN_MODEM));  /* default mode, but library may have overridden, needed for BLE at same time as wifi */
    nimble_port_init();
 
@@ -296,26 +262,53 @@ void app_main()
    /* Start the task */
    nimble_port_freertos_init(ble_task);
 
+   jo_t jo_device(device_t * d) {
+      jo_t j = jo_object_alloc();
+      jo_string(j, "address", ble_addr_format(&d->addr));
+      jo_string(j, "name", d->name);
+      return j;
+   }
+
    /* main loop */
    while (1)
    {
       usleep(100000);
       uint32_t now = uptime();
-      // Devices missing
+         // Devices missing
          for (device_t * d = device; d; d = d->next)
-            if (!d->missing && d->last + (d->apple ? missingtime : 300) < now)
+            if (!d->missing && d->last + missingtime < now)
             {                   // Missing
                d->missing = 1;
-               d->connected = 0;
+               jo_t j = jo_device(d);
+               revk_event("missing", &j);
                ESP_LOGI(TAG, "Missing %s", ble_addr_format(&d->addr));
             }
-      // Devices found
-      for (device_t * d = device; d; d = d->next)
-         if (d->found)
-         {
-            d->found = 0;
-            ESP_LOGI(TAG, "Found %s", ble_addr_format(&d->addr));
-         }
+         // Devices found
+         for (device_t * d = device; d; d = d->next)
+            if (d->found)
+            {
+               d->found = 0;
+               jo_t j = jo_device(d);
+               revk_event("found", &j);
+               ESP_LOGI(TAG, "Found %s", ble_addr_format(&d->addr));
+            }
+         // Reporting
+         for (device_t * d = device; d; d = d->next)
+            if (!d->missing && (d->lastreport + reporting < now || d->tempreport + temprise < d->temp))
+            {
+               jo_t j = jo_device(d);
+               if (d->temp < 0)
+                  jo_litf(j, "temp", "-%d.%02d", (-d->temp) / 100, (-d->temp) % 100);
+               else
+                  jo_litf(j, "temp", "%d.%02d", d->temp / 100, d->temp % 100);
+               jo_int(j, "rssi", d->rssi);
+               // TODO bat
+               revk_info("report", &j);
+               d->lastreport = now;
+               d->tempreport = d->temp;
+               ESP_LOGI(TAG, "Report %s", ble_addr_format(&d->addr));
+               // TODO listening and only reporting if we are better rssi than other reports
+            }
 
       if (!ble_gap_disc_active())
       {                         // Restart discovery, but first should be safe to check for deletions
